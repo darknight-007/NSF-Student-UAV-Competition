e6a851d0592cde2610c5eb185f02f99b5acd975f
jdas <jnaneshwar.das@gmail.com>
2016-09-19 23:06:39 -0400

    readding copy of sitl_gazebo, unsubmoduled

---
+++ sitl_gazebo/Build/CMakeFiles/rotors_gazebo_mavlink_interface.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mrsl/src/Firmware/Tools/sitl_gazebo/src/gazebo_mavlink_interface.cpp" "/home/mrsl/src/Firmware/Tools/sitl_gazebo/Build/CMakeFiles/rotors_gazebo_mavlink_interface.dir/src/gazebo_mavlink_interface.cpp.o"
+  "/home/mrsl/src/Firmware/Tools/sitl_gazebo/src/geo_mag_declination.cpp" "/home/mrsl/src/Firmware/Tools/sitl_gazebo/Build/CMakeFiles/rotors_gazebo_mavlink_interface.dir/src/geo_mag_declination.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mrsl/src/Firmware/Tools/sitl_gazebo/Build/msgs/CMakeFiles/mav_msgs.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "../include"
+  "/usr/include/gazebo-6.6"
+  "/usr/include/sdformat-3.7"
+  "/usr/include/ignition/math2"
+  "/usr/include/OGRE"
+  "/usr/include/OGRE/Terrain"
+  "/usr/include/OGRE/Paging"
+  "/usr/include/gazebo-6.6/gazebo/msgs"
+  "/usr/include/eigen3"
+  "msgs"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
